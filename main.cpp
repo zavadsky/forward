7a154fc75276567365364254fd03f3624fc2423f
zavadsky <zava@ukr.net>
2023-11-26 11:50:02 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -22,11 +22,11 @@ int main(int argc, char** argv) {
         if(argc==2 || strcmp(argv[2], "-r") == 0) { // Forward Replacement Arithmetic
             w = new WordTextReplacement(ifname);  // Pre-process the text
             enc = new ArithmeticEncoderReplacement((WordTextReplacement*)w,bout); // Create the arithmetic encoder
-            r = new RMD({0,2,30},50000); //Generating the set of the RMD(1,3-inf) codewords
+            r = new RMD({0,2,30},500000); //Generating the set of the RMD(1,3-inf) codewords
         } else {
             w = new WordBasedText(ifname);  // Pre-process the text
             enc = new ArithmeticEncoderForward(w,bout); // Create the arithmetic encoder
-            r = new RMD({0,3,30},50000); //Generating the set of the RMD(1-3,5-inf) codewords
+            r = new RMD({0,3,30},500000); //Generating the set of the RMD(1-3,5-inf) codewords
         }
         if(argc>3 && strcmp(argv[3], "-e") == 0)
             enc->emulate();
